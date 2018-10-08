1eac92a0c651103b1edf0137580339cf34e0281c
lol987lol <43947456+lol987lol@users.noreply.github.com>
2018-10-08 08:39:41 +0200

    Update calculator.java
    Delete unused Auto Implement

--- src/calculator.java
+++ src/calculator.java
@@ -3,7 +3,7 @@ import javax.script.ScriptException;
 public class Calculator {
 
 	public static void main(String[] args)throws ScriptException {
-		// TODO Auto-generated method stub
+
 		calculatorGui gui = new calculatorGui();
 		gui.pack();
 	}
